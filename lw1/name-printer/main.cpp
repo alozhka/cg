d2e1fbc0ba395f94c10eb45d906f42ab5bd7dd34
Sergey Lozhkin <alozhka@yahoo.com>
2026-02-02 12:25:10 +0300

    refactor: добавил настройку позиции и цвета для буквы

--- lw1/name-printer/main.cpp
+++ lw1/name-printer/main.cpp
@@ -27,75 +27,78 @@ int main()
 	return 0;
 }
 
-void DrawLetterC(sf::RenderWindow& window)
+void DrawLetterC(sf::RenderWindow& window, sf::Vector2f leftTop, sf::Color color)
 {
 	sf::RectangleShape leftRect(sf::Vector2f(50, 300));
-	leftRect.setPosition(sf::Vector2f(50, 100));
-	leftRect.setFillColor(sf::Color::Black);
+	leftRect.setPosition(leftTop);
+	leftRect.setFillColor(color);
 	window.draw(leftRect);
 
 	sf::RectangleShape aboveRect(sf::Vector2f(200, 50));
-	aboveRect.setPosition(sf::Vector2f(50, 100));
-	aboveRect.setFillColor(sf::Color::Black);
+	aboveRect.setPosition(leftTop);
+	aboveRect.setFillColor(color);
 	window.draw(aboveRect);
 
 	sf::RectangleShape lowerRect(sf::Vector2f(200, 50));
-	lowerRect.setPosition(sf::Vector2f(50, 350));
-	lowerRect.setFillColor(sf::Color::Black);
+	lowerRect.setPosition(sf::Vector2f(leftTop.x, leftTop.y + 250));
+	lowerRect.setFillColor(color);
 	window.draw(lowerRect);
 }
 
-void DrawLetterA(sf::RenderWindow& window)
+void DrawLetterA(sf::RenderWindow& window, sf::Vector2f leftTop, sf::Color color)
 {
 	sf::RectangleShape leftRect(sf::Vector2f(50, 300));
-	leftRect.setPosition(sf::Vector2f(300, 100));
-	leftRect.setFillColor(sf::Color::Black);
+	leftRect.setPosition(leftTop);
+	leftRect.setFillColor(color);
 	window.draw(leftRect);
 
 	sf::RectangleShape rightRect(sf::Vector2f(50, 300));
-	rightRect.setPosition(sf::Vector2f(450, 100));
-	rightRect.setFillColor(sf::Color::Black);
+	rightRect.setPosition(sf::Vector2f(leftTop.x + 150, leftTop.y));
+	rightRect.setFillColor(color);
 	window.draw(rightRect);
 
 	sf::RectangleShape aboveRect(sf::Vector2f(150, 50));
-	aboveRect.setPosition(sf::Vector2f(300, 100));
-	aboveRect.setFillColor(sf::Color::Black);
+	aboveRect.setPosition(leftTop);
+	aboveRect.setFillColor(color);
 	window.draw(aboveRect);
 
 	sf::RectangleShape lowerRect(sf::Vector2f(150, 50));
-	lowerRect.setPosition(sf::Vector2f(300, 200));
-	lowerRect.setFillColor(sf::Color::Black);
+	lowerRect.setPosition(sf::Vector2f(leftTop.x, leftTop.y + 150));
+	lowerRect.setFillColor(color);
 	window.draw(lowerRect);
 }
 
-void DrawLetterL(sf::RenderWindow& window)
+void DrawLetterL(sf::RenderWindow& window, sf::Vector2f leftTop, sf::Color color)
 {
 	sf::RectangleShape aboveRect(sf::Vector2f(150, 50));
-	aboveRect.setPosition(sf::Vector2f(600, 100));
-	aboveRect.setFillColor(sf::Color::Black);
+	aboveRect.setPosition(leftTop);
+	aboveRect.setFillColor(color);
 	window.draw(aboveRect);
 
 	sf::RectangleShape leftRect(sf::Vector2f(50, 300));
-	leftRect.setPosition(sf::Vector2f(600, 100));
-	leftRect.setFillColor(sf::Color::Black);
+	leftRect.setPosition(leftTop);
+	leftRect.setFillColor(color);
 	window.draw(leftRect);
 
 	sf::RectangleShape lowerRect(sf::Vector2f(100, 50));
-	lowerRect.setPosition(sf::Vector2f(550, 350));
-	lowerRect.setFillColor(sf::Color::Black);
+	lowerRect.setPosition(sf::Vector2f(leftTop.x - 50, leftTop.y + 250));
+	lowerRect.setFillColor(color);
 	window.draw(lowerRect);
 
 	sf::RectangleShape rightRect(sf::Vector2f(50, 300));
-	rightRect.setPosition(sf::Vector2f(700, 100));
-	rightRect.setFillColor(sf::Color::Black);
+	rightRect.setPosition(sf::Vector2f(leftTop.x + 100, leftTop.y));
+	rightRect.setFillColor(color);
 	window.draw(rightRect);
 }
 
 void DrawInitials(sf::RenderWindow& window)
 {
-	DrawLetterC(window);
-	DrawLetterA(window);
-	DrawLetterL(window);
+	sf::Vector2f leftTopC{ 50, 100 };
+	DrawLetterC(window, leftTopC, sf::Color::Black);
+	sf::Vector2f leftTopA{ 300, 100 };
+	DrawLetterA(window, leftTopA, sf::Color::Magenta);
+	sf::Vector2f leftTopL{ 600, 100 };
+	DrawLetterL(window, leftTopL, sf::Color::Cyan);
 	window.display();
 }
 
