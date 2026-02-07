2d2ae1279152c96b4b3a10664d73323e1f8778a6
Sergey Lozhkin <alozhka@yahoo.com>
2026-02-07 22:25:04 +0300

    feat: нарисовал Эрика Картмана (no refactor)ˆ

---
+++ lw1/picture/main.cpp
@@ -0,0 +1,56 @@
+#include <functional>
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <optional>
+#include "EricCartman.hpp"
+
+constexpr float GROUND_Y = 450.0f;
+
+sf::RenderWindow CreateWindow()
+{
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 16;
+    return sf::RenderWindow{
+        sf::VideoMode(sf::Vector2u(800, 600)),
+        "Eric Cartman",
+        sf::Style::Default,
+        sf::State::Windowed,
+        settings
+    };
+}
+
+int MainLoop(const std::function<void(sf::RenderWindow& window)>& onDraw)
+{
+    sf::RenderWindow window = CreateWindow();
+
+    while (window.isOpen())
+    {
+        while (const std::optional<sf::Event> event = window.pollEvent())
+        {
+            if (event.has_value() && event->is<sf::Event::Closed>())
+            {
+                window.close();
+                break;
+            }
+        }
+
+        window.clear(sf::Color::White);
+        onDraw(window);
+        window.display();
+    }
+
+    return 0;
+}
+
+int main()
+{
+    EricCartman cartman;
+    cartman.setPosition(sf::Vector2f(400.f, 300.f));
+    cartman.setScale(sf::Vector2f(1.5f, 1.5f));
+
+    auto onDraw = [&](sf::RenderWindow& window) {
+        window.draw(cartman);
+    };
+
+    return MainLoop(onDraw);
+}
