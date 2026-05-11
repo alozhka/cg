c67040d2a627e6a9efe196bf1a518c76e47c079a
Sergey Lozhkin <alozhka@yahoo.com>
2026-05-11 21:57:07 +0300

    feat: написал движение кораблей в модели

--- lw6/sea_battle/src/sea_battle/model/SeaBattleGame.hpp
+++ lw6/sea_battle/src/sea_battle/model/SeaBattleGame.hpp
@@ -2,11 +2,50 @@
 #include "Ship.hpp"
 #include "Torpedo.hpp"
 
+#include <graphics/shared/Randomizer.hpp>
 #include <vector>
 
 class SeaBattleGame
 {
+public:
+	static constexpr float SPAWN_X = -30.0f;
+	static constexpr float DESPAWN_X = 30.0f;
+	static constexpr float WATER_Y = 0.0f;
+	static constexpr float MIN_Z = -8.0f;
+	static constexpr float MAX_Z = 8.0f;
+	static constexpr float SPAWN_INTERVAL = 2.5f;
+
+	void Update(float dt)
+	{
+		m_spawnTimer -= dt;
+		if (m_spawnTimer <= 0.0f)
+		{
+			SpawnRandomShip();
+			m_spawnTimer = SPAWN_INTERVAL;
+		}
+
+		for (auto& ship : m_ships)
+		{
+			ship->Update(dt);
+		}
+
+		std::erase_if(m_ships, [](const ShipPtr& ship) {
+			return ship->IsOutOfBounds(DESPAWN_X);
+		});
+	}
+
+	const std::vector<ShipPtr>& GetShips() const { return m_ships; }
+
 private:
+	void SpawnRandomShip()
+	{
+		const glm::vec3 position{ SPAWN_X, WATER_Y, Randomizer::RandomRange(MIN_Z, MAX_Z) };
+		const auto type = static_cast<ShipType>(static_cast<int>(Randomizer::RandomRange(0, 3)));
+
+		m_ships.push_back(std::make_shared<Ship>(type, position));
+	}
+
 	std::vector<ShipPtr> m_ships;
 	TorpedoPtr m_torpedo;
+	float m_spawnTimer = 0.0f;
 };
--- lw6/sea_battle/src/sea_battle/model/Ship.hpp
+++ lw6/sea_battle/src/sea_battle/model/Ship.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <glm/glm.hpp>
 #include <memory>
 
 enum class ShipState
@@ -7,23 +8,70 @@ enum class ShipState
 	Drowning
 };
 
+enum class ShipType
+{
+	Cruiser,
+	Battleship,
+	Frigate
+};
+
+inline float GetShipSpeed(ShipType type)
+{
+	switch (type)
+	{
+	case ShipType::Cruiser:
+		return 3.0f;
+	case ShipType::Battleship:
+		return 1.8f;
+	case ShipType::Frigate:
+		return 4.5f;
+	}
+	return 0.0f;
+}
+
 class Ship;
 using ShipPtr = std::shared_ptr<Ship>;
 
 class Ship
 {
+public:
+	Ship(ShipType type, const glm::vec3& position)
+		: m_type(type)
+		, m_position(position)
+		, m_speed(GetShipSpeed(type))
+	{
+	}
+
+	void Update(float dt)
+	{
+		m_position.x += m_speed * dt;
+	}
+
+	ShipType GetType() const
+	{
+		return m_type;
+	}
+	const glm::vec3& GetPosition() const
+	{
+		return m_position;
+	}
+	float GetSpeed() const
+	{
+		return m_speed;
+	}
+	ShipState GetState() const
+	{
+		return m_state;
+	}
+
+	bool IsOutOfBounds(float maxX) const
+	{
+		return m_position.x > maxX;
+	}
+
 private:
+	ShipType m_type;
+	glm::vec3 m_position;
+	float m_speed;
 	ShipState m_state = ShipState::Alive;
-};
-
-class Cruiser : public Ship
-{
-};
-
-class Battleship : public Ship
-{
-};
-
-class Frigate : public Ship
-{
 };
